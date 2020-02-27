0d0cace1d8378f5ec581e2d54331122e859ae83b
Nanda Gopal <ngopal253@gmail.com>
2020-02-27 20:19:27 +0530

    transaction model added

---
+++ controllers/transactions.js
@@ -0,0 +1,34 @@
+const Transaction = require('../models/Transaction')
+
+// @desc    Get all transactions
+// @route   GET /api/v1/transactions
+// @access  Public
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find()
+    return res.staus(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    })
+  }
+}
+
+// @desc    Add a transaction
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransaction = async (req, res, next) => {
+  res.send('POST Transaction')
+}
+
+// @desc    Delete transaction
+// @route   DELETE /api/v1/transactions/:id
+// @access  Public
+exports.deleteTransaction = async (req, res, next) => {
+  res.send('DELETE Transaction')
+}
